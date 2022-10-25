d734809519ab5017530e2ab4ec474e07812d1f4c
Dipta Dhananjaya <madedipta88@gmail.com>
2022-10-25 20:59:05 +0800

    banner

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.css";
+import { Container } from "react-bootstrap";
+import HeroImageSmall from "../../assets/hero-1.png";
+import HeroImageBig from "../../assets/hero-2.png";
+
+const Home = () => {
+  return (
+    <div className="bg-color">
+      <Container>
+        <div className="row py-5">
+          <div className="col-12 col-lg-6 content-wrapper">
+            <h1 className="title-home">Kami Penuhi Kebutuhan Pangan Anda</h1>
+            <p className="content">
+              Kami penuh kebutuhan pangan anda dengan produk yang kami sediakan
+              dengan <br />
+              dengan harga bersahabat dan kualitas yang terjamin
+            </p>
+            <button className="btn-start">Produk Kami</button>
+          </div>
+          <div className="col-12 col-lg-6 d-flex hero-container gap-3">
+            <img src={HeroImageSmall} alt="" className="img-fluid hero-small" />
+            <img src={HeroImageBig} alt="" className="img-fluid hero-big" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
